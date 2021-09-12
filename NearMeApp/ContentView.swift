ee8e2353a516563a4fef9dafe65733571dfc0d4f
Elman <elmanasadi1377@gmail.com>
2021-09-12 14:48:02 +0430

    implement search functionality

--- NearMeApp/ContentView.swift
+++ NearMeApp/ContentView.swift
@@ -6,14 +6,52 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     
-    @ObservedObject private var locationManager = LocationManager() 
+    @ObservedObject private var locationManager = LocationManager()
+    @State private var search : String = ""
     
     var body: some View {
-        MapView() 
+        
+        ZStack(alignment: .top) {
+            
+            MapView()
+            
+            TextField("Search", text: self.$search, onEditingChanged: {_ in}) {
+                getNearByLandMarks()
+            }.textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+            .offset(y: 44)
+            
+        }.edgesIgnoringSafeArea(.all)
+        
     }
+    
+    private func getNearByLandMarks() {
+        
+        guard let location = self.locationManager.location else { return }
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = self.search
+        request.region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        
+        
+        let search = MKLocalSearch(request: request)
+        
+        search.start { response, error in
+            guard let response = response , error == nil else {return}
+            
+            let mapItems = response.mapItems
+            
+            mapItems.map {
+                print($0.placemark)
+            }
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- NearMeApp/Coordinators/Coordinator.swift
+++ NearMeApp/Coordinators/Coordinator.swift
@@ -17,4 +17,20 @@ final class Coordinator: NSObject, MKMapViewDelegate {
         self.control = control
     }
     
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        
+        if let annotationView = views.first {
+            if let annotaion = annotationView.annotation {
+                if annotaion is MKAnnotation {
+                    
+                    let region = MKCoordinateRegion(center: annotaion.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+                    
+                    mapView.setRegion(region, animated: true)
+                    
+                }
+            }
+        }
+        
+    }
+    
 }
